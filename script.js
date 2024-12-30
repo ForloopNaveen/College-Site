999fec3b0b5bd975907a0583dfc85d0a2dd99fa4
Naveen-kumar-1 <codemavericknk@gmail.com>
2024-12-31 00:37:52 +0530

    50% completed

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ document.getElementById("menu").addEventListener("click", () => {
 });
 document.getElementById("close").addEventListener("click", () => {
   document.getElementById("nav-links").style.right = "-200px";
-
 });
+function selectedCourse() {
+  var selectedOption = document.getElementById("course-select").value;
+
+  var allCourses = document.querySelectorAll(".course-col");
+
+  allCourses.forEach(function (course) {
+    if (course.id.includes(selectedOption)) {
+      course.style.display = "block";
+    } else {
+      course.style.display = "none";
+    }
+  });
+}
